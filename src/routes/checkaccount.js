b5280177726373ab8f8605541723e0e9639d996e
Ari Maulana Firmansyah <65420719+arimaulanafirmansyah@users.noreply.github.com>
2022-08-07 11:08:46 +0700

    Update checkaccount.js

--- src/routes/checkaccount.js
+++ src/routes/checkaccount.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { checkAccountAOV, checkAccountCOD, checkAccountFF, checkAccountGI, checkAccountML, checkAccountSM } from '../controller/checkaccountController.js'
+import { checkAccountAOV, checkAccountCOD, checkAccountFF, checkAccountHDI, checkAccountGI, checkAccountML, checkAccountSM } from '../controller/checkaccountController.js'
 
 const checkaccount = express()
 
 checkaccount.get('/ff/:id', checkAccountFF)
+checkaccount.get('/hdi/:id', checkAccountHDI)
 checkaccount.get('/ml/:zoneid/:id', checkAccountML)
 checkaccount.get('/aov/:id', checkAccountAOV)
 checkaccount.get('/cod/:id', checkAccountCOD)
