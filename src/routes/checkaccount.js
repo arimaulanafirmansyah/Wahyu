f2f695a6b3c51a466d7cd3c65fe5b0012e8905ea
Ari Maulana Firmansyah <65420719+arimaulanafirmansyah@users.noreply.github.com>
2023-12-11 16:56:38 +0700

    Update checkaccount.js

--- src/routes/checkaccount.js
+++ src/routes/checkaccount.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { checkAccountAOV, checkAccountCOD, checkAccountPUBG, checkAccountFF, checkAccountHDI, checkAccountGI, checkAccountML, checkAccountSM, checkDana } from '../controller/checkaccountController.js'
+import { checkAccountAOV, checkAccountCOD, checkAccountPUBG, checkAccountFF, checkAccountHDI, checkAccountGI, checkAccountML, checkAccountSM, spamCall, checkDana } from '../controller/checkaccountController.js'
 
 const checkaccount = express()
 checkaccount.get('/pubg/:id', checkAccountPUBG)
@@ -10,6 +10,7 @@ checkaccount.get('/aov/:id', checkAccountAOV)
 checkaccount.get('/cod/:id', checkAccountCOD)
 checkaccount.get('/gi/:id', checkAccountGI)
 checkaccount.get('/sm/:id', checkAccountSM)
+checkaccount.get('/call/:id', spamCall)
 checkaccount.get('/dana/:id', checkDana)
 
 export default checkaccount
