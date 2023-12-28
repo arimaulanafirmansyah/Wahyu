6c749c508614241443e12365576caf66cf0be3e8
Ari Maulana Firmansyah <65420719+arimaulanafirmansyah@users.noreply.github.com>
2023-12-29 01:20:45 +0700

    Update checkaccount.js

--- src/routes/checkaccount.js
+++ src/routes/checkaccount.js
@@ -2,6 +2,15 @@ import express from 'express'
 import { checkAccountAOV, checkAccountCOD, checkAccountPUBG, checkAccountFF, checkAccountHDI, checkAccountGI, checkAccountML, checkAccountSM, spamCall } from '../controller/checkaccountController.js'
 
 const checkaccount = express()
+
+let totalVisitors = 0; // Counter for total visitors
+
+// Middleware to increment total visitor count for all routes
+checkaccount.use((req, res, next) => {
+    totalVisitors++;
+    next();
+});
+
 checkaccount.get('/pubg/:id', checkAccountPUBG)
 checkaccount.get('/ff/:id', checkAccountFF)
 checkaccount.get('/hdi/:id', checkAccountHDI)
@@ -12,4 +21,4 @@ checkaccount.get('/gi/:id', checkAccountGI)
 checkaccount.get('/sm/:id', checkAccountSM)
 checkaccount.get('/call/:id', spamCall)
 
-export default checkaccount
+export { checkaccount, totalVisitors };
